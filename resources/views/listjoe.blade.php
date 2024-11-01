cc21ba0481367f994ad488d026d92c4f4f520f4b
Jonah Klimack <jonahklimackk@gmail.com>
2024-11-01 11:13:34 -0400

    splash page

---
+++ resources/views/listjoe.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LRB7WDQ3CJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LRB7WDQ3CJ');
+</script>
+
+    <body>
+<center>
+<h1> Listjoe Recommends... </h1>
+
+<br><br>
+<h2>
+    <a href="">
+    Earn $20/day or more <br>
+doing  <br><i>one
+simple thing... </i></h2>
+</a>
+
+</center>
+
+
+<br><br>
+
+
+
+    </body>
+
+</head>
+</html>
